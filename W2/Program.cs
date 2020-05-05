c2889a28baa850198ea15d2ddfe9efe37bc986f9
Chemist-lab <60918051+Chemist-lab@users.noreply.github.com>
2020-05-05 21:27:06 +0300

    Lab32

---
+++ W2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace W2
+{
+    class Program
+    {
+        static string fileRead = @"f.txt";
+        static string fileWrite = @"g.txt";
+        static string readNums;
+        static int CheckNum;
+        static void Main(string[] args)
+        {
+            
+            StreamReader reader = new StreamReader(fileRead);
+            StreamWriter writer = new StreamWriter(fileWrite, false, System.Text.Encoding.Default);
+            readNums = reader.ReadToEnd();
+
+            for(int i = 0; i < readNums.Length; i++)
+            {
+                CheckNum = readNums[i];
+                if (CheckNum % 2 == 0)
+                {
+                    Console.WriteLine(readNums[i]);
+                    writer.Write(readNums[i]);
+                }
+            }
+            writer.Close();
+        }
+    }
+}
